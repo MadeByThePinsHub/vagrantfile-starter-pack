e180a132f85e8095efbd40e52b133346a05a6c99
Andrei Jiroh Eugenio Halili <andreijiroh@madebythepins.tk>
2021-04-09 20:12:19 +0800

    script-toolkits: Finalize script for testing

--- script-toolkits/dogehouse-init-dev-env.sh
+++ script-toolkits/dogehouse-init-dev-env.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 ########## RabbitMQ ##########
-# Copy-pasta from https://www.rabbitmq.com/install-debian.html#apt-quick-start
-# In this file, we replaced bionic with focal to reflect to the Vargant box that
-# we want to use, in this case, the official cloud image of Ubuntu 20.04 from Canonical
+## Copy-pasta from https://www.rabbitmq.com/install-debian.html#apt-quick-start
+## In this file, we replaced bionic with focal to reflect to the Vargant box that
+## we want to use, in this case, the official cloud image of Ubuntu 20.04 from Canonical
 sudo apt-get install curl gnupg debian-keyring debian-archive-keyring apt-transport-https -y
 
 ## Team RabbitMQ's main signing key
@@ -42,6 +42,9 @@ sudo apt-get install -y erlang-base \
 ## Install rabbitmq-server and its dependencies
 sudo apt-get install rabbitmq-server -y --fix-missing
 
+## ensure the service is up
+sudo systemctl enable --now rabbitmq-server
+
 ########## Node.js and friends ##########
 curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get install -y nodejs
@@ -50,7 +53,25 @@ sudo apt-get install -y nodejs
 sudo npm install -g yarn
 
 ########## PostgresSQL ##########
-# Instructions are based on https://www.postgresqltutorial.com/install-postgresql-linux/
+## Instructions are based on https://www.postgresqltutorial.com/install-postgresql-linux/
 echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-sudo apt-get update && sudo apt-get install postgres
+sudo apt-get update && sudo apt-get install postgres -y
+
+## ensure the server is up
+sudo systemctl enable --now postgresql
+
+## Init database
+sudo -u postgres psql -c "CREATE DATABASE kousa_repo2" postgres
+
+########### Elixir ##########
+## Since we installed Erlang in the erailer stage for RabbitMQ
+## We can just install Elixir stuff instead of doing the Erlang install again
+wget -O "/tmp/erlang-solutions_2.0_all.deb" https://packages.erlang-solutions.com/erlang-solutions_2.0_all.deb
+sudo dpkg -i /tmp/erlang-solutions_2.0_all.deb
+
+## let's do the cache update again, maybe do this one after
+## adding repos instead later
+sudo apt-get update
+## blame the install guide (https://elixir-lang.org/install.html) but we should be fine
+udo apt-get install elixir -y
